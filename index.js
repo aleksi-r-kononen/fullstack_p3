9d52a0aa04077999c7abf900e4f8324adacfd941
Aleksi Kononen <aleksi.r.kononen@aalto.fi>
2024-04-26 18:34:49 +0300

    uibuild

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body) {
@@ -79,23 +79,24 @@ app.post('/api/persons', (request, response) => {
   })
 
   console.log(`adding ${body.name} to phonebook`)
-  person.save().then(savedPerson => {
-    console.log(`addition of ${body.name} was successful`)
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      console.log(`addition of ${body.name} was successful`)
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidator: true, context: 'query' }
+  )
     .then(updatedPerson => {
-      console.log(`updating number of ${person.name} was successful`)
+      console.log(`updating number of ${name} was successful`)
       response.json(updatedPerson)
     })
     .catch(error => next(error))
@@ -106,6 +107,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') { 
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
