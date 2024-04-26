59cb239d9b385be11ac97958b99fdb7e6b19e2fa
Aleksi Kononen <aleksi.r.kononen@aalto.fi>
2024-04-26 17:53:57 +0300

    Solution to P3c

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('cnt', function getCnt (req) {
   const body = req.body
@@ -16,57 +18,42 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time :cnt'))
 
-let people = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
 
 app.get('/info', (request, response) => {
-  const txt = `
-    <p>Phonebook has ${people.length} names</p>
-    <p>${Date()}</p>
-  `
-  response.send(txt)
+  Person.find({}).then(ps => {
+    const txt = `
+      <p>Phonebook has ${ps.length} names</p>
+      <p>${Date()}</p>
+    `
+    response.send(txt)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = people.find(p => p.id === id)
-
-  if (!person)
-    return response.status(404).end()
-
-  response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(people)
+  console.log("fetching people for /api/persons from mongoDB")
+  Person.find({}).then(ps => {
+    response.json(ps)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const removed_lad = people.find(p => p.id === id)
-  people = people.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -84,24 +71,52 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (people.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({
-      error: "name must be unique"
-    })
-  }
+  /* Should ensure name is unique */
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  console.log(`adding ${body.name} to phonebook`)
+  person.save().then(savedPerson => {
+    console.log(`addition of ${body.name} was successful`)
+    response.json(savedPerson)
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 26042024)
   }
 
-  people = people.concat(person)
-
-  response.json(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      console.log(`updating number of ${person.name} was successful`)
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
